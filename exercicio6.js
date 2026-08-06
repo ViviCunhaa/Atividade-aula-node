f91ca73c11e1ba843b54f7c5fb61106ef8c7d45f
--replace-all <--replace-all>
2026-08-06 16:03:00 -0300

    Exercicio 6

--- exercicio6.js
+++ exercicio6.js
@@ -1,2 +1,9 @@
+const idades = [12, 18, 25, 40, 16, 22];
 
+for (let i = 0; i < idades.length; i++) {
+    if (idades[i] >= 18) {
+        console.log('Você é maior de idade.');
+    } else {
+        console.log('Você é menor de idade.');
+    }
 }
