7758bcf6da3f485fa604cc49c67adac7052906c8
--replace-all <--replace-all>
2026-08-06 15:36:03 -0300

    Exercicio 3

--- Exercicio3.js
+++ Exercicio3.js
@@ -2,6 +2,9 @@
 
 const capitais = ['Brasília', 'Salvador', 'Manaus', 'Recife'];
 console.log("Verifique se Manaus está na lista de capitais");
-console.log("Manaus")
 
-
+if (capitais.includes('Manaus')) {
+    console.log('Manaus está na lista de capitais');
+} else {
+    console.log('Manaus não está na lista de capitais');
+}
