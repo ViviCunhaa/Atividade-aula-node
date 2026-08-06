7cf9e69706840a914136c2f9815ffd28da3916fa
--replace-all <--replace-all>
2026-08-06 15:46:35 -0300

    Exercicio 4

--- Exercicio3.js
+++ Exercicio3.js
@@ -1,5 +1,3 @@
-// Buscar Capital
-
 const capitais = ['Brasília', 'Salvador', 'Manaus', 'Recife'];
 console.log("Verifique se Manaus está na lista de capitais");
 
---
+++ Exercicio4.js
@@ -0,0 +1,2 @@
+const numeros = [5, 9, 12, 4, 18, 15];
+console.log("mostre apenas:"12, 15, 18);
